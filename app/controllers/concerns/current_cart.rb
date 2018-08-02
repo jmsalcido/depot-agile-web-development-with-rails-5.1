307bc8433fb85beb0d09747c5d43ca6f273e4baa
Jose Salcido <jose@otfusion.org>
2018-08-02 11:43:20 -0700

    add the cart addition

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -3,10 +3,8 @@ module CurrentCart
 
   def set_cart
     @cart = Cart.find(session[:cart_id])
-  rescue ActiveRecord::RecordNotFound
-    @cart = Cart.create
-    session[:cart_id] = @card.id
-    # wouldnt it be better to search in the session first?
-    # instead of recovering from an Exception?... ohwell...
+    rescue ActiveRecord::RecordNotFound
+      @cart = Cart.create
+      session[:cart_id] = @cart.id
   end
 end
