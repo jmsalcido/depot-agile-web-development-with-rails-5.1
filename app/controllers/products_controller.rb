c72765a3e02ee97233e6d29a01b358c31117dc3f
Jose Salcido <jose@otfusion.org>
2018-08-05 12:32:37 -0700

    add ActionCable to update prices to all the browsers connected when there is an update in the catalog

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -44,6 +44,11 @@ class ProductsController < ApplicationController
       if @product.update(product_params)
         format.html { redirect_to @product, notice: 'Product was successfully updated.' }
         format.json { render :show, status: :ok, location: @product }
+
+        @products = Product.order(:title)
+        ActionCable.server.broadcast 'products',
+                                     html: render_to_string('store/index', layout: false)
+
       else
         format.html { render :edit }
         format.json { render json: @product.errors, status: :unprocessable_entity }
