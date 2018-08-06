7e0bc4814ee2df1fcc50766bde4095f7942d5592
Jose Salcido <jose@otfusion.org>
2018-08-05 17:25:14 -0700

    add who_bought atom feed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :line_items
   resources :carts
   root 'store#index', as: 'store_index'
-  resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products do
+    get :who_bought, on: :member
+  end
 end
