55cf0e634df521de918f3c0657cefe21a3e23261
Jose Salcido <jose@otfusion.org>
2018-08-16 16:36:07 -0700

    send logic of 'pago' usage to `order.rb`

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
 
+  require 'pago'
+
   has_many :line_items, dependent: :destroy
   belongs_to :pay_type
 
@@ -10,6 +12,41 @@ class Order < ApplicationRecord
     cart.line_items.each do |item|
       item.cart_id = nil
       line_items << item
+    end
+  end
+
+  def charge!(pay_type_params)
+    payment_details = {}
+    payment_method = nil
+
+    case pay_type.name
+    when 'Check'
+      payment_method = :check
+      payment_details[:routing] = pay_type_params[:routing_number]
+      payment_details[:account] = pay_type_params[:account_number]
+    when 'Credit Card'
+      payment_method = :credit_card
+      month, year = pay_type_params[:expiration_date].split(//)
+      payment_details[:cc_num] = pay_type_params[:credit_card_number]
+      payment_details[:expiration_month] = month
+      payment_details[:expiration_year] = year
+    when 'Purchase Order'
+      payment_method = :po
+      payment_details[:po_num] = pay_type_params[:po_number]
+    else
+      raise ArgumentError('Foo')
+    end
+
+    payment_result = Pago.make_payment(
+        order_id: id,
+        payment_method: payment_method,
+        payment_details: payment_details
+    )
+
+    if payment_result.succeeded?
+      OrderMailer.received(self).deliver_later
+    else
+      raise payment_result.error
     end
   end
 
