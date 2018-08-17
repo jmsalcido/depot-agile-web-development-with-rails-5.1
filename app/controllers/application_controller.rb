f92a4a37f891fff4752da8cd27b0de7142eefa16
Jose Salcido <jose@otfusion.org>
2018-08-16 17:37:34 -0700

    add a comment on how to send Exceptions as part of an email (I dont want to do it)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  rescue_from Exception, with: :send_email_to_administrator
+
+  private
+
+  def send_email_to_administrator(exception)
+    logger.warn exception
+    # comment, I dont want to send emails each time I receive an exception, also, this is just the OrderMailer, not needed.
+    # OrderMailer.received(Order.last).deliver_later
+    throw exception
+  end
+
 end
