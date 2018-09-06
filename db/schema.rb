6056de7eaa59f027e68cd76e6e207cc4116643e8
Jose Salcido <jose@otfusion.org>
2018-09-06 11:03:54 -0700

    add users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_17_000521) do
+ActiveRecord::Schema.define(version: 2018_08_31_193208) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -52,6 +52,13 @@ ActiveRecord::Schema.define(version: 2018_08_17_000521) do
     t.text "description"
     t.string "image_url"
     t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
